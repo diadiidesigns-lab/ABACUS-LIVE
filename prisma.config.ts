493877bb40cbfc90dab7a6202c8f1813adb97062
David Enyichukwu Chidi-Emmanuel <davidce@Davids-MacBook-Pro.local>
2026-05-06 03:24:47 -0400

    fix: prisma 7 configuration and supabase connection
    fix: prisma 7 configuration and supabase connection#

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL,
-  },
-  migrate: {
-    url: process.env.DATABASE_URL,
-    directUrl: process.env.DIRECT_URL,
+    url: process.env.DIRECT_URL,
   },
 });
